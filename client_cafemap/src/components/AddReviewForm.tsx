91cae48b71c4ecb9d692c53c2a573f0b7ef8c596
ubutharun <tharun242424@gmail.com>
2023-11-15 04:19:37 +0530

    add review form

--- client_cafemap/src/components/AddReviewForm.tsx
+++ client_cafemap/src/components/AddReviewForm.tsx
@@ -1,9 +1,58 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import RestaurantFinder from "../apis/RestaurantFinder";
+import { useParams } from "react-router-dom";
+import ReviewSchema from "../types/ReviewSchema";
+import { RestaurantsContext } from "../context/RestaurantsContext";
 
 const AddReviewForm = () => {
+  //Current restaurant
+  const { id } = useParams();
+
+  const { selectedRestaurantData, setSelectedRestaurantData } =
+    useContext(RestaurantsContext);
+
   const [name, setName] = useState("");
-  const [rating, setRating] = useState("");
+  const [rating, setRating] = useState(0); //zero => dummy
   const [reviewText, setReviewText] = useState("");
+
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    if (name.trim() === "" || rating === 0 || reviewText.trim() === "") {
+      return;
+    }
+
+    console.log(`clicked submit review on restaurant id:`, id);
+
+    try {
+      const response = await RestaurantFinder.post(`/${id}/addreview`, {
+        name: name,
+        rating: rating,
+        review: reviewText,
+      });
+      console.log("response is", response);
+
+      const ParsedNewReview = ReviewSchema.parse(response.data.data.review);
+
+      console.log("Parsed New review is", ParsedNewReview);
+
+      //changing context for SelectedRestaurantData
+      //should add the new review in RestaurantDetailPage
+
+      //PROBLEM: that tutor guy says,
+      //this will cause problem when updating average review count
+      //he says do a hack to make it work but this itself works fine
+
+      //Im thinking we will do another get restaurant call to db to get fresh restaurant
+      //To update that too if we need
+      setSelectedRestaurantData({
+        ...selectedRestaurantData,
+        reviews: [...selectedRestaurantData.reviews, ParsedNewReview],
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="flex justify-center">
@@ -24,15 +73,17 @@ const AddReviewForm = () => {
             <label htmlFor="rating">Rating</label>
             <select
               value={rating}
-              onChange={(e) => setRating(e.target.value)}
+              onChange={(e) => setRating(parseInt(e.target.value))}
               id="rating"
             >
-              <option disabled>rating</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
+              <option value={0} disabled>
+                rate here
+              </option>
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={4}>4</option>
+              <option value={5}>5</option>
             </select>
           </div>
         </div>
@@ -46,7 +97,9 @@ const AddReviewForm = () => {
           />
         </div>
         <div className="flex justify-center">
-          <button className="flex justify-center w-fit">Submit</button>
+          <button className="flex justify-center w-fit" onClick={handleSubmit}>
+            Submit
+          </button>
         </div>
       </form>
     </div>

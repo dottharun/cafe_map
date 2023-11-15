9383f6d764de76bf6c45d07e538befec2937bc50
ubutharun <tharun242424@gmail.com>
2023-11-15 12:56:58 +0530

    ui changes

--- client_cafemap/src/components/Header.tsx
+++ client_cafemap/src/components/Header.tsx
@@ -1,7 +1,12 @@
 const Header = () => {
   return (
     <>
-      <h1 className="font-light flex justify-center">Restaurant Finder</h1>
+      <div className="flex justify-center p-4 pb-10">
+        <div className="">
+          <h1 className="font-mono italic pb-2">Restaurant Finder</h1>
+          <p className="text-xs">Write your experiences below 🌺</p>
+        </div>
+      </div>
     </>
   );
 };

d181314d1035e01b05b9dc598fe276298983c4d7
ubutharun <tharun242424@gmail.com>
2023-11-16 22:47:52 +0530

    render backend

--- client_cafemap/src/apis/RestaurantFinder.ts
+++ client_cafemap/src/apis/RestaurantFinder.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const RestaurantFinder = axios.create({
-  baseURL: "http://localhost:3006/api/v1/restaurants",
+  baseURL: "https://cafemap-server.onrender.com/api/v1/restaurants",
 });
 
 export default RestaurantFinder;

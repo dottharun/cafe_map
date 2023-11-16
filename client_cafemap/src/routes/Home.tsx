88781fab3a635a584ff4d7917ee421e8b68222c4
ubutharun <tharun242424@gmail.com>
2023-11-16 21:30:27 +0530

    vercel try5

--- client_cafemap/src/routes/Home.tsx
+++ client_cafemap/src/routes/Home.tsx
@@ -2,25 +2,12 @@ import AddRestaurant from "../components/AddRestaurant";
 import Header from "../components/Header";
 import RestaurantList from "../components/RestaurantList";
 
-import ritvik from "../assets/ritvik2.jpg";
-
 const Home = () => {
   return (
-    <div
-      style={{
-        backgroundImage: `url(${ritvik})`,
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-        width: "100%",
-        height: "100vh",
-      }}
-    >
-      <div className="flex flex-col">
-        <Header />
-        <AddRestaurant />
-        <RestaurantList />
-      </div>
+    <div className="flex flex-col">
+      <Header />
+      <AddRestaurant />
+      <RestaurantList />
     </div>
   );
 };

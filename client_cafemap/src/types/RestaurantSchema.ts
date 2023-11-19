34242669e756ec57dc8939ebe3e53db43bf0f219
ubutharun <tharun242424@gmail.com>
2023-11-19 13:58:49 +0530

    smol fix

--- client_cafemap/src/types/RestaurantSchema.ts
+++ client_cafemap/src/types/RestaurantSchema.ts
@@ -11,7 +11,7 @@ const RestaurantSchema = z.object({
     z.literal(4),
     z.literal(5),
   ]),
-  rating_count: z.optional(z.union([z.number(), z.null()])),
+  rating_count: z.optional(z.union([z.number().nonnegative(), z.null()])),
   average_rating: z.optional(z.union([z.number().min(0).max(5), z.null()])),
 });
 
